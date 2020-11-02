bd17e218388ef56ddf1d6436ecb46ce730aa60b6
Jenniffer <jenlpac@yahoo.com>
2020-11-02 00:14:02 -0600

    include base for portfolio page

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+        <section>
+            <h1>Title</h1>
+            <p>Technologies</p>
+        </section>
+    );
+}
+
+export default Portfolio;
